151608bd6c69274fc80de83d80280142082d8d17
Oleg <incognitorus@gmail.com>
2024-12-01 02:04:16 +0400

    update

--- src/modules/strategies/dhm/dhm.strategy.process.service.ts
+++ src/modules/strategies/dhm/dhm.strategy.process.service.ts
@@ -230,7 +230,7 @@ export class DhmStrategyProcessService {
       const params = {
         stop_loss: this.getFib(session, stopLevel),
         take_profit: this.getFib(session, profitLevel),
-        //post_only: true,
+        post_only: true,
         // tp_trigger_by: 'LastPrice', // Опционально, тип цены для срабатывания TP
         // sl_trigger_by: 'LastPrice', // Опционально, тип цены для срабатывания SL
         // time_in_force: 'GoodTillCancel', // Время действия ордера
