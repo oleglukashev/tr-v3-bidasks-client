8ea462bb6fab63db5afa4e942762b5bdc7c6e5eb
Oleg <incognitorus@gmail.com>
2025-04-20 00:13:12 +0400

    fix

--- src/modules/strategies/dhm/dhm.strategy.detect.cron.service.ts
+++ src/modules/strategies/dhm/dhm.strategy.detect.cron.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import * as yargs from 'yargs';
 import { Cron } from '@nestjs/schedule';
-import { KlinesPrismaService } from '../../klinesPrisma/klinesPrisma.service';
 import { DhmStrategyDetectService } from './dhm.strategy.detect.service';
-import { startOfHourAgoTs, startOfHourTs } from '../../../utils/time';
+import { startOfHourAgoTs } from '../../../utils/time';
 import { StrategySessionsEntityService } from '../../entity-services/strategy-sessions-entity-service';
 import { direction } from '../../../utils/kline';
+import { KlinesEntityService } from '../../entity-services/klines-entity-service';
 
 @Injectable()
 export class DhmStrategyDetectCronService {
   constructor(
     private readonly dhmStrategyDetectService: DhmStrategyDetectService,
     private readonly strategySessoinsEntityService: StrategySessionsEntityService,
-    private readonly klinesPrismaService: KlinesPrismaService,
+    private readonly klinesEntityService: KlinesEntityService,
   ) {}
 
   @Cron('*/5 * * * * *')
@@ -22,33 +22,45 @@ export class DhmStrategyDetectCronService {
     const argv: any = yargs.argv;
     const startCurrentHourTs = startOfHourAgoTs();
 
-    const pairsLastKlines: any = await this.klinesPrismaService.$queryRaw`
-      SELECT k.id, k.low, k.high, k.ts
-      FROM klines k
-      INNER JOIN pairs p ON p.id = k.pair_id
-      WHERE k.interval = 60 AND k.ts = ${startCurrentHourTs} AND k.pair_id = ${argv.pairId}::integer LIMIT 1;
-    `;
+    const lastKline: any = await this.klinesEntityService.findFirst({
+      where: {
+        interval: 60,
+        ts: startCurrentHourTs,
+        pairId: parseInt(argv.pairId),
+      },
+    });
+
+    if (!lastKline) {
+      console.log(`No last kline`);
+      return;
+    }
+
+    // const pairsLastKlines: any = await this.klinesPrismaService.$queryRaw`
+    //   SELECT k.id, k.low, k.high, k.ts, k.pair_id
+    //   FROM klines k
+    //   WHERE k.interval = 60 AND k.ts = ${startCurrentHourTs} AND k.pair_id = ${argv.pairId}::integer LIMIT 1;
+    // `;
     // INTERVAL 60m!!!!
 
-    for (const kline of pairsLastKlines) {
-      const directionValue = direction(kline);
-      const existTriggeredStrategySessionWithDirection =
-        await this.strategySessoinsEntityService.findFirst({
-          where: {
-            status: { in: ['waiting', 'triggered'] },
-            pairId: { equals: parseInt(argv.pairId) },
-            direction: { equals: directionValue },
-          },
-        });
+    //for (const kline of pairsLastKlines) {
+    const directionValue = direction(lastKline);
+    const existTriggeredStrategySessionWithDirection =
+      await this.strategySessoinsEntityService.findFirst({
+        where: {
+          status: { in: ['waiting', 'triggered'] },
+          pairId: { equals: parseInt(argv.pairId) },
+          direction: { equals: directionValue },
+        },
+      });
 
-      if (existTriggeredStrategySessionWithDirection) {
-        console.log(
-          `Triggered strategy sessions with pair id ${argv.pairId} and direction ${directionValue} already exist`,
-        );
-        continue;
-      }
-
-      await this.dhmStrategyDetectService.detect(kline.id, followDirection);
+    if (existTriggeredStrategySessionWithDirection) {
+      console.log(
+        `Triggered strategy sessions with pair id ${argv.pairId} and direction ${directionValue} already exist`,
+      );
+      return;
     }
+
+    await this.dhmStrategyDetectService.detect(lastKline.id, followDirection);
+    //}
   }
 }
