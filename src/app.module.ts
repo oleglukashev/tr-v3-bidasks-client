53861431ec6fbe397e476388a719813151f70317
Oleg <incognitorus@gmail.com>
2025-04-21 13:24:33 +0400

    add nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { GeneralPrismaModule } from './modules/generalPrisma/generalPrisma.modul
 import { KlinesPrismaModule } from './modules/klinesPrisma/klinesPrisma.module';
 import { ApiKlinesModule } from './modules/api/v1/klines/klines.module';
 import { EntityModule } from './modules/entity-services/entities.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     EntityModule,
     GeneralPrismaModule,
     KlinesPrismaModule,
