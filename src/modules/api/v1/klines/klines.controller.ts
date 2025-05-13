0301dbbc541c598bea4f546f99a637c83b335f05
Oleg <incognitorus@gmail.com>
2025-05-13 14:01:44 +0400

    update

--- src/modules/api/v1/klines/klines.controller.ts
+++ src/modules/api/v1/klines/klines.controller.ts
@@ -37,4 +37,21 @@ export class ApiKlinesController {
     items = items.reverse();
     return items;
   }
+
+  @Get('by_pair_id_and_tf_and_ts')
+  @ApiOkResponse({ description: 'Get kline by pair_id, tf and ts' })
+  @HttpCode(HttpStatus.OK)
+  public async byPairIdAndTfAndTs(
+    @Query('pairId', new DefaultValuePipe(false), ParseIntPipe) pairId,
+    @Query('tf', new DefaultValuePipe(false), ParseIntPipe) tf,
+    @Query('ts', new DefaultValuePipe(false), ParseIntPipe) ts,
+  ): Promise<any> {
+    return this.klinesEntityService.findFirst({
+      where: {
+        pairId,
+        interval: tf,
+        ts,
+      },
+    });
+  }
 }
