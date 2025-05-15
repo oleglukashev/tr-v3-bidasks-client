5fdcfebd75c70eff958a49851a67daa1764b35ad
Oleg <incognitorus@gmail.com>
2025-05-15 15:07:29 +0400

    update

--- src/modules/api/generate-fpp/generate-fpp.service.ts
+++ src/modules/api/generate-fpp/generate-fpp.service.ts
@@ -114,12 +114,12 @@ export class GenerateFppService {
           parseFloat(cluster1Poc.p) < parseFloat(kline1.close),
         );
         console.log(
-          'c2 poc > k2 open',
-          parseFloat(cluster2Poc.p) > parseFloat(kline2.open),
+          'c2 poc < k2 close',
+          parseFloat(cluster2Poc.p) < parseFloat(kline2.close),
         );
         if (
           parseFloat(cluster1Poc.p) < parseFloat(kline1.close) &&
-          parseFloat(cluster2Poc.p) > parseFloat(kline2.open)
+          parseFloat(cluster2Poc.p) < parseFloat(kline2.close)
         ) {
           await this.fppEntityService.baseCreate({
             ts: kline2.ts,
