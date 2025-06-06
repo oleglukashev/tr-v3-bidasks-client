db1709308b8a16ce1daacc42d4cec8eb99e65011
Oleg Lukashev <incognitorus@gmail.com>
2025-06-07 00:33:19 +0300

    update

--- src/modules/generate-fpp/generate-fpp.service.ts
+++ src/modules/generate-fpp/generate-fpp.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import moment from 'moment';
-import { direction, pocFromCluster } from '../../utils/kline';
-import { ClustersEntityService } from '../entity-services/clusters-entity-service';
+import {
+  delta,
+  direction,
+  pocFromCluster,
+  sortedClusterData,
+} from '../../utils/kline';
+// import { ClustersEntityService } from '../entity-services/clusters-entity-service';
 import { FppEntityService } from '../entity-services/fpp-entity-service';
 import { InjectRedis } from '@nestjs-modules/ioredis';
 import Redis from 'ioredis';
@@ -10,7 +15,7 @@ import { getCluster, getClusterKeyByPairIdTsTf } from '../../utils/redis';
 @Injectable()
 export class GenerateFppService {
   constructor(
-    private readonly clustersEntityService: ClustersEntityService,
+    // private readonly clustersEntityService: ClustersEntityService,
     private readonly fppEntityService: FppEntityService,
     @InjectRedis('bidasksDb') private readonly redis: Redis,
   ) {}
@@ -55,6 +60,7 @@ export class GenerateFppService {
     const kline2Res = await fetch(this.getKlinePath(pairId, cluster2.ts, tf));
     const kline1 = await kline1Res.json();
     const kline2 = await kline2Res.json();
+    // Interception pattern
     await this.processInterceptionPattern(
       cluster1,
       cluster2,
@@ -63,6 +69,19 @@ export class GenerateFppService {
       pairId,
       tf,
     );
+    // Reverse pattern
+    await this.processReversePattern(
+      cluster1,
+      cluster2,
+      kline1,
+      kline2,
+      pairId,
+      tf,
+    );
+    // Locked volume
+    await this.processLockedVolumePattern(cluster2, kline2, pairId, tf);
+    // Locked delta
+    await this.processLockedDeltaPattern(cluster2, kline2, pairId, tf);
   }
 
   private async processInterceptionPattern(
@@ -99,6 +118,7 @@ export class GenerateFppService {
             pairId,
             tf,
             direction: 'down',
+            type: 'interception',
           });
         }
       } else {
@@ -112,8 +132,162 @@ export class GenerateFppService {
             pairId,
             tf,
             direction: 'up',
+            type: 'interception',
           });
         }
+      }
+    }
+  }
+
+  private async processReversePattern(
+    cluster1: any,
+    cluster2: any,
+    kline1: any,
+    kline2: any,
+    pairId: number,
+    tf: number,
+  ) {
+    if (!kline1 || !kline2) {
+      console.log(`${pairId},${tf}: Not enough klines data`);
+    }
+
+    const cluster1Poc: any = pocFromCluster(cluster1);
+    const cluster2Poc: any = pocFromCluster(cluster2);
+
+    if (!cluster1Poc || !cluster2Poc) {
+      console.log(`${pairId},${tf}: Not enough poc data`);
+    }
+
+    const kline1Direction = direction(kline1);
+    const kline2Direction = direction(kline2);
+
+    if (kline1Direction !== kline2Direction) {
+      if (kline1Direction === 'up') {
+        // down reverse
+        if (parseFloat(cluster1Poc.p) > parseFloat(cluster2Poc.p)) {
+          await this.fppEntityService.baseCreate({
+            ts: kline2.ts,
+            pairId,
+            tf,
+            direction: 'down',
+            type: 'reverse',
+          });
+        }
+      } else {
+        // up reverse
+        if (parseFloat(cluster2Poc.p) > parseFloat(cluster1Poc.p)) {
+          await this.fppEntityService.baseCreate({
+            ts: kline2.ts,
+            pairId,
+            tf,
+            direction: 'up',
+            type: 'reverse',
+          });
+        }
+      }
+    }
+  }
+
+  private async processLockedVolumePattern(
+    cluster: any,
+    kline: any,
+    pairId: number,
+    tf: number,
+  ) {
+    if (!kline) {
+      console.log(`${pairId},${tf}: Not enough kline data`);
+    }
+
+    const clusterPoc: any = pocFromCluster(cluster);
+
+    if (!clusterPoc) {
+      console.log(`${pairId},${tf}: Not enough poc data`);
+    }
+
+    const klineDirection = direction(kline);
+
+    if (klineDirection === 'down') {
+      // down reverse
+      if (parseFloat(clusterPoc.p) > parseFloat(kline.open)) {
+        await this.fppEntityService.baseCreate({
+          ts: kline.ts,
+          pairId,
+          tf,
+          direction: 'down',
+          type: 'locked_volume',
+        });
+      }
+    } else {
+      // up reverse
+      if (parseFloat(clusterPoc.p) < parseFloat(kline.open)) {
+        await this.fppEntityService.baseCreate({
+          ts: kline.ts,
+          pairId,
+          tf,
+          direction: 'up',
+          type: 'locked_volume',
+        });
+      }
+    }
+  }
+
+  private async processLockedDeltaPattern(
+    cluster: any,
+    kline: any,
+    pairId: number,
+    tf: number,
+  ) {
+    if (!kline) {
+      console.log(`${pairId},${tf}: Not enough kline data`);
+    }
+
+    const clusterPoc: any = pocFromCluster(cluster);
+
+    if (!clusterPoc) {
+      console.log(`${pairId},${tf}: Not enough poc data`);
+    }
+
+    const klineDirection = direction(kline);
+
+    if (klineDirection === 'down') {
+      // down reverse
+      const sortedData = sortedClusterData(cluster, false);
+      const firstClusterPriceDelta = parseFloat(delta(sortedData[0]));
+      const secondClusterPriceDelta = parseFloat(delta(sortedData[1]));
+      const thirdClusterPriceDelta = parseFloat(delta(sortedData[2]));
+      if (
+        firstClusterPriceDelta > 0 &&
+        secondClusterPriceDelta > 0 &&
+        thirdClusterPriceDelta > 0 &&
+        parseFloat(sortedData[2].p) > parseFloat(kline.open)
+      ) {
+        await this.fppEntityService.baseCreate({
+          ts: kline.ts,
+          pairId,
+          tf,
+          direction: 'down',
+          type: 'locked_volume',
+        });
+      }
+    } else {
+      // up reverse
+      const sortedData = sortedClusterData(cluster, true);
+      const firstClusterPriceDelta = parseFloat(delta(sortedData[0]));
+      const secondClusterPriceDelta = parseFloat(delta(sortedData[1]));
+      const thirdClusterPriceDelta = parseFloat(delta(sortedData[2]));
+      if (
+        firstClusterPriceDelta < 0 &&
+        secondClusterPriceDelta < 0 &&
+        thirdClusterPriceDelta < 0 &&
+        parseFloat(sortedData[2].p) < parseFloat(kline.open)
+      ) {
+        await this.fppEntityService.baseCreate({
+          ts: kline.ts,
+          pairId,
+          tf,
+          direction: 'up',
+          type: 'locked_volume',
+        });
       }
     }
   }
--- src/utils/kline.ts
+++ src/utils/kline.ts
@@ -1,3 +1,11 @@
+function sortByPrice(array: any[], asc = true): any {
+  return [...array].sort((a: any, b: any) => {
+    return asc
+      ? parseFloat(a.p) - parseFloat(b.p)
+      : parseFloat(b.p) - parseFloat(a.p);
+  });
+}
+
 export function direction(kline: any) {
   return parseFloat(kline.close) > parseFloat(kline.open) ? 'up' : 'down';
 }
@@ -10,7 +18,14 @@ export function pocFromCluster(cluster: any) {
   return Object.entries(cluster.data).reduce((max, entry) => {
     const [, current]: any = entry;
     const [, maxValue]: any = max;
-
     return parseFloat(current.v) > parseFloat(maxValue.v) ? entry : max;
   })[1];
+}
+
+export function sortedClusterData(cluster: any, asc = true) {
+  return sortByPrice(Object.values(cluster.data), asc);
+}
+
+export function delta(clusterPrice: any) {
+  return (parseFloat(clusterPrice.bv) - parseFloat(clusterPrice.sv)).toString();
 }
