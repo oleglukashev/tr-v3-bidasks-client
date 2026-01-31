b2043b3114a9565c67a32a4bd691a53ec0b96565
Oleg Lukashev <incognitorus@gmail.com>
2026-01-31 11:59:27 +0400

    update

--- src/modules/generate-fpp/generate-fpp.cron.service.ts
+++ src/modules/generate-fpp/generate-fpp.cron.service.ts
@@ -11,35 +11,35 @@ import sleep from '../../utils/sleep';
 export class GenerateFppCronService {
   constructor(private readonly generateFppService: GenerateFppService) {}
 
-  @Cron('* * * * *')
-  async handleEveryMinuteCron() {
-    const argv: any = yargs.argv;
-    const tradingServiceId: string = argv['tradingServiceId'];
-    const tradingServiceData = config[tradingServiceId];
-    const now = startOfMinuteTs();
-    await sleep(10000);
-    for (const pairId in tradingServiceData.types.future.tickers) {
-      await this.generateFppService.processFpp(parseInt(pairId), 1);
-
-      if (now === getStartTsByTf(now, 5)) {
-        await this.generateFppService.processFpp(parseInt(pairId), 5);
-      }
-
-      if (now === getStartTsByTf(now, 15)) {
-        await this.generateFppService.processFpp(parseInt(pairId), 15);
-      }
-
-      if (now === getStartTsByTf(now, 30)) {
-        await this.generateFppService.processFpp(parseInt(pairId), 30);
-      }
-
-      if (now === getStartTsByTf(now, 60)) {
-        await this.generateFppService.processFpp(parseInt(pairId), 60);
-      }
-
-      if (now === getStartTsByTf(now, 240)) {
-        await this.generateFppService.processFpp(parseInt(pairId), 240);
-      }
-    }
-  }
+  // @Cron('* * * * *')
+  // async handleEveryMinuteCron() {
+  //   const argv: any = yargs.argv;
+  //   const tradingServiceId: string = argv['tradingServiceId'];
+  //   const tradingServiceData = config[tradingServiceId];
+  //   const now = startOfMinuteTs();
+  //   await sleep(10000);
+  //   for (const pairId in tradingServiceData.types.future.tickers) {
+  //     await this.generateFppService.processFpp(parseInt(pairId), 1);
+  //
+  //     if (now === getStartTsByTf(now, 5)) {
+  //       await this.generateFppService.processFpp(parseInt(pairId), 5);
+  //     }
+  //
+  //     if (now === getStartTsByTf(now, 15)) {
+  //       await this.generateFppService.processFpp(parseInt(pairId), 15);
+  //     }
+  //
+  //     if (now === getStartTsByTf(now, 30)) {
+  //       await this.generateFppService.processFpp(parseInt(pairId), 30);
+  //     }
+  //
+  //     if (now === getStartTsByTf(now, 60)) {
+  //       await this.generateFppService.processFpp(parseInt(pairId), 60);
+  //     }
+  //
+  //     if (now === getStartTsByTf(now, 240)) {
+  //       await this.generateFppService.processFpp(parseInt(pairId), 240);
+  //     }
+  //   }
+  // }
 }
